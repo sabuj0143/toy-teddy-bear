8d087f28d62b0f4cf461282ba37400156a76f276
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-18 20:45:29 +0600

    ExtraSection2 added

--- src/components/ExtraSection/ExtraSection1.jsx
+++ src/components/ExtraSection/ExtraSection1.jsx
@@ -7,7 +7,7 @@ import img4 from '../../../public/categoryImages/classicCuddlers.jpg'
 const ExtraSection1 = () => {
     return (
         <>
-            <div className="carousel w-full h-screen">
+            <div className="carousel w-[100%] h-screen">
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={img1} className="w-[100%] h-[100vh]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -16,21 +16,21 @@ const ExtraSection1 = () => {
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src={img2} className="w-full h-[100vh]" />
+                    <img src={img2} className="w-[100%] h-[100vh]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src={img3} className="w-full h-[100vh]" />
+                    <img src={img3} className="w-[100%] h-[100vh]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
-                    <img src={img4} className="w-full h-[100vh]" />
+                    <img src={img4} className="w-[100%] h-[100vh]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,15 +4,17 @@ import Banner from './Banner';
 import Gallery from './Gallery';
 import Category from '../Category/Category';
 import ExtraSection1 from '../ExtraSection/ExtraSection1';
+import ExtraSection2 from '../ExtraSection/ExtraSection2';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <ExtraSection2></ExtraSection2>
             <Outlet></Outlet>
+            {/* <ExtraSection1></ExtraSection1> */}
             {/* <Gallery></Gallery> */}
-            <ExtraSection1></ExtraSection1>
         </div>
     );
 };
