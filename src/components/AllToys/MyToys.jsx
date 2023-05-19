a237110d81e5efc5855cd248d0d9150bc37c1460
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-19 18:50:51 +0600

    Delete method added

--- src/components/AllToys/MyToys.jsx
+++ src/components/AllToys/MyToys.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import SingleTeddy from './SingleTeddy';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const MyToys = () => {
 
+    const {user} = useContext(AuthContext);
     const [teddys, setTeddys] = useState([]);
     console.log(teddys);
 
     useEffect(() => {
-        fetch('http://localhost:5000/teddys')
+        fetch(`http://localhost:5000/teddys?email=${user?.email}`)
             .then(res => res.json())
             .then(data => setTeddys(data))
     }, [])
@@ -15,11 +17,13 @@ const MyToys = () => {
     return (
         <div>
             <h2>This is My Toys: {teddys.length}</h2>
-            <div className='mt-4'>
+            <div className='w-full gap-4 md:grid grid-cols-1'>
                 {
                     teddys.map(teddy => <SingleTeddy
                         key={teddy._id}
                         teddy={teddy}
+                        setTeddys={setTeddys}
+                        teddys={teddys}
                     >
                     </SingleTeddy>)
                 }
--- src/components/AllToys/SingleTeddy.jsx
+++ src/components/AllToys/SingleTeddy.jsx
@@ -1,31 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
-const SingleTeddy = ({ teddy }) => {
+const SingleTeddy = ({ teddy, teddys, setTeddys }) => {
     const { _id, price, toyName, photo, sellerName, rating, description } = teddy;
+
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/teddys/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Teddy has been deleted.',
+                                'success'
+                            )
+                            const remaining = teddys.filter(toy => toy._id !== _id)
+                            setTeddys(remaining)
+                        }
+                    })
+
+            }
+        });
+
+
+
+    };
+
     return (
-        <div className='w-[100%]'>
-
-            <div className="flex flex-col w-[80%] mx-auto items-center justify-between mt-4 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-
+        <div className="card card-side w-[50%] mx-auto bg-base-100 shadow-xl items-center">
+            <figure><img className="w-[300px] h-[200px] p-8 rounded" src={photo} alt="Movie" /></figure>
+            <div className="flex justify-between w-full pr-4">
                 <div>
-                    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={photo} alt="" />
+                    <h2 className="card-title">ToyName : {toyName}</h2>
+                    <p> SellerName : {sellerName}</p>
+                    <p> Price : {price}</p>
+                    <p> Rating : {rating}</p>
+                    <p> Description : {description}</p>
                 </div>
-
-                <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{sellerName}</h5>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{toyName}</h5>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{price}</h5>
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{rating}</h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+                <div className="card-actions justify-end">
+                    <div className="btn-group btn-group-vertical space-y-4">
+                        <Link>
+                            <button className="btn btn-outline btn-info">Update</button>
+                        </Link>
+                        <button onClick={() => handleDelete(_id)} className="btn btn-outline btn-error">Delete</button>
+                    </div>
                 </div>
-                <div>
-                    <button>E</button>
-                    <button>D</button>
-                </div>
-
             </div>
-
-        </ div>
+        </div>
     );
 };
 
